e6c5cdede1d48c83ed8aac31c4746512acc715bc
Postmodern <postmodern.mod3@gmail.com>
2023-12-18 15:14:54 -0800

    Added missing specs.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -11,6 +11,8 @@ describe CommandKit::Completion::Task do
   let(:tempfile)    { Tempfile.new(['command_kit-completion','.sh']) }
   let(:output_file) { tempfile.path }
 
+  let(:fixtures_dir) { File.join(__dir__,'fixtures') }
+
   subject do
     described_class.new(
       class_file:  class_file,
@@ -218,20 +220,255 @@ describe CommandKit::Completion::Task do
     end
 
     context "when the command class includes CommandKit::Commands" do
+      class TestCommandWithSubCommands < CommandKit::Command
+        include CommandKit::Commands
+
+        option :global_option, short: '-g',
+                               desc:  'A global option'
+
+        class Foo < CommandKit::Command
+
+          option :foo_opt1, desc: 'Foo option 1'
+
+          option :foo_opt2, value: {
+                              type: String
+                            },
+                            desc: 'Foo option 2'
+
+        end
+
+        class Bar < CommandKit::Command
+
+          option :bar_opt1, desc: 'Bar option 1'
+
+          option :bar_opt2, value: {
+                              type: String
+                            },
+                            desc: 'Bar option 2'
+
+        end
+
+        command_name 'test'
+        command Foo
+        command Bar
+
+      end
+
+      let(:command_class) { TestCommandWithSubCommands }
+
+      it "must add completion rules for the other commands" do
+        expect(subject.completion_rules_for(command_class)).to eq(
+          {
+            "test"     => %w[--global-option help foo bar],
+            "test foo" => %w[--foo-opt1 --foo-opt2],
+            "test bar" => %w[--bar-opt1 --bar-opt2]
+          }
+        )
+      end
+
+      context "when the command has command aliases" do
+        class TestCommandWithSubCommandsAndCommandAliases < CommandKit::Command
+          include CommandKit::Commands
+
+          option :global_option, short: '-g',
+                                 desc:  'A global option'
+
+          class Foo < CommandKit::Command
+
+            option :foo_opt1, desc: 'Foo option 1'
+
+            option :foo_opt2, value: {
+                                type: String
+                              },
+                              desc: 'Foo option 2'
+
+          end
+
+          class Bar < CommandKit::Command
+
+            option :bar_opt1, desc: 'Bar option 1'
+
+            option :bar_opt2, value: {
+                                type: String
+                              },
+                              desc: 'Bar option 2'
+
+          end
+
+          command_name 'test'
+          command Foo
+          command Bar
+
+          command_aliases['foo2'] = 'foo'
+          command_aliases['bar2'] = 'bar'
+
+        end
+
+        let(:command_class) { TestCommandWithSubCommandsAndCommandAliases }
+
+        it "must include the command aliases in the completion rules" do
+          expect(subject.completion_rules_for(command_class)).to eq(
+            {
+              "test"     => %w[--global-option help foo bar foo2 bar2],
+              "test foo" => %w[--foo-opt1 --foo-opt2],
+              "test bar" => %w[--bar-opt1 --bar-opt2]
+            }
+          )
+        end
+      end
+
       context "but when one of the commands does not define any options" do
-        it "must omit the command from the completion rules"
+        class TestCommandWithSubCommandsWithNoOptions < CommandKit::Command
+          include CommandKit::Commands
+
+          option :global_option, short: '-g',
+                                 desc:  'A global option'
+
+          class Foo < CommandKit::Command
+
+            option :foo_opt1, desc: 'Foo option 1'
+
+            option :foo_opt2, value: {
+                                type: String
+                              },
+                              desc: 'Foo option 2'
+
+          end
+
+          class Bar < CommandKit::Command
+          end
+
+          command_name 'test'
+          command Foo
+          command Bar
+
+        end
+
+        let(:command_class) { TestCommandWithSubCommandsWithNoOptions }
+
+        it "must omit the command from the completion rules" do
+          expect(subject.completion_rules_for(command_class)).to eq(
+            {
+              "test"     => %w[--global-option help foo bar],
+              "test foo" => %w[--foo-opt1 --foo-opt2]
+            }
+          )
+        end
       end
 
       context "and when one of the sub-commands also includes CommandKit::Commands" do
+        class TestCommandWithSubSubCommands < CommandKit::Command
+          include CommandKit::Commands
+
+          option :global_option, short: '-g',
+                                 desc:  'A global option'
+
+          class Foo < CommandKit::Command
+
+            option :foo_opt1, desc: 'Foo option 1'
+
+            option :foo_opt2, value: {
+                                type: String
+                              },
+                              desc: 'Foo option 2'
+
+          end
+
+          class Bar < CommandKit::Command
+
+            include CommandKit::Commands
+
+            option :bar_opt1, desc: 'Bar option 1'
+
+            option :bar_opt2, value: {
+                                type: String
+                              },
+                              desc: 'Bar option 2'
+
+            class Baz < CommandKit::Command
+
+              option :baz_opt1, desc: 'Baz option 1'
+
+              option :baz_opt2, value: {
+                                  type: String
+                                },
+                                desc: 'Baz option 2'
+
+            end
+
+            class Qux < CommandKit::Command
+
+              option :qux_opt1, desc: 'Qux option 1'
+
+              option :qux_opt2, value: {
+                                  type: String
+                                },
+                                desc: 'Qux option 2'
+
+            end
+
+            command Baz
+            command Qux
+
+          end
+
+          command_name 'test'
+          command Foo
+          command Bar
+
+        end
+
+        let(:command_class) { TestCommandWithSubSubCommands }
+
+        it "must recursively include completion rules for the sub-sub-commands" do
+          expect(subject.completion_rules_for(command_class)).to eq(
+            {
+              "test"     => %w[--global-option help foo bar],
+              "test foo" => %w[--foo-opt1 --foo-opt2],
+              "test bar" => %w[--bar-opt1 --bar-opt2 help baz qux],
+              "test bar baz" => %w[--baz-opt1 --baz-opt2],
+              "test bar qux" => %w[--qux-opt1 --qux-opt2]
+            }
+          )
+        end
       end
     end
   end
 
   describe "#completion_rules" do
-    it "must load the class from #class_file and return the generated completion rules for it"
+    it "must load the class from #class_file and return the generated completion rules for it" do
+      expect(subject.completion_rules).to eq(
+        {
+          "foo" => %w[--config-file help config list update ls up],
+          "foo config" => %w[help get set],
+          "foo update" => %w[--quiet],
+          "foo*--config-file" => %w[<file>]
+        }
+      )
+    end
 
     context "when #input_file is set" do
-      it "must merge the additional completion rules with the generated ones"
+      let(:input_file) { File.join(fixtures_dir,'additional_rules.yml') }
+
+      subject do
+        described_class.new(
+          class_file:  class_file,
+          class_name:  class_name,
+          output_file: output_file,
+          input_file:  input_file
+        )
+      end
+
+      it "must merge the additional completion rules with the generated ones" do
+        expect(subject.completion_rules).to eq(
+          {
+            "foo" => %w[--config-file help config list update ls up],
+            "foo config" => %w[help get set],
+            "foo update" => ['--quiet', '$(foo list)'],
+            "foo*--config-file" => %w[<file>]
+          }
+        )
+      end
     end
   end
 end
